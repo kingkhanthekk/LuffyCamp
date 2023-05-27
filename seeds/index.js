1701365e025d1a6e146ad33b7a031b70fe9302bf
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-05-27 19:15:11 +0300

    Complete model, added images, description, and price to campgrounds. Changed details route co cop up with new data.

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const name = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
+    let price = Math.floor(Math.random() * 20) + 10;
     let camp = new Campground({
       title: `${name(descriptors)} ${name(places)}`,
       location: `${seeds[i].city}, ${seeds[i].state}`,
+      image: "https://source.unsplash.com/random/?campground",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro rerum ipsa facilis nesciunt alias praesentium recusandae impedit quis necessitatibus explicabo. Fugit sed natus autem officiis officia, facilis doloribus. Neque, error?",
+      price,
     });
     await camp.save();
   }

2a251bbee7e0fdd86d394436fbf5e79a7aca5c45
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-06-05 18:17:05 +0300

    Users can now register to the web

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const catchError = require("../utils/catchError");
 
 router.use(express.urlencoded({ extended: true }));
 
@@ -8,8 +9,20 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  "/register",
+  catchError(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const user = await new User({ username, email });
+      await User.register(user, password);
+      req.flash("success", "Welcome to LuffyCamp!");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/register");
+    }
+  })
+);
 
 module.exports = router;
